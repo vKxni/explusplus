270f264043980204e0615ca635ea0ec82301be1d
vKxni <74613145+vKxni@users.noreply.github.com>
2022-05-20 17:29:23 +0200

    dropped test

--- home/test/home_test.exs
+++ home/test/home_test.exs
@@ -2,7 +2,7 @@ defmodule HomeTest do
   use ExUnit.Case
   doctest Home
 
-  test "greets the world" do
-    assert Home.hello() == :world
-  end
+ # test "greets the world" do
+ #   assert Home.hello() == :world
+ # end
 end
